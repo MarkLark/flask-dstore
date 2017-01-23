3d923c93b5e66febf939861281f970ec734acf25
MarkLark <mark.pittaway@mlit.net.au>
2017-01-23 16:25:22 +1100

    Initial commit of project files

---
+++ run-test-server.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+if __name__ == '__main__':
+    from .tests.app import create_app
+    from .tests.models import Car
+    app, api = create_app( config = "tests.configs.server" )
+
+    with app.app_context( ):
+        api.store.create_all()
+
+        Car( manufacturer = "Holden", make = "Commodore", year = 2005 ).add()
+        Car( manufacturer = "Holden", make = "Commodore", year = 2006 ).add()
+        Car( manufacturer = "Holden", make = "Commodore", year = 2007 ).add()
+        Car( manufacturer = "Holden", make = "Commodore", year = 2008 ).add()
+        Car( manufacturer = "Holden", make = "Commodore", year = 2009 ).add()
+
+    app.run( host = "0.0.0.0", port = 5000, debug = True, use_reloader = False )
+
+    with app.app_context():
+        api.store.destroy_all()
---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+from os import path
+from codecs import open
+
+root_dir = path.abspath(path.dirname(__file__))
+
+with open( path.join( root_dir, 'README.rst' ), encoding = 'utf-8' ) as f:
+    long_description = f.read()
+
+
+setup(
+    name='Flask-DStore',
+    version='0.1.0a1',
+    url = 'https://github.com/MarkLark/flask-dstore',
+    license='MIT',
+    author='Mark Pittaway',
+    author_email='mark.pittaway@mlit.net.au',
+    description='DStore Web API and JS Client using FLask',
+    long_description=long_description,
+    packages=['flask_dstore'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask>=0.7',
+        'dstore',
+        'dstore-acl'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Database :: Front-Ends'
+    ]
+)
