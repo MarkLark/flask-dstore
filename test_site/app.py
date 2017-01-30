9f70d820cca68b20a8263f40cc3ba8d590de46f6
MarkLark <mark.pittaway@mlit.net.au>
2017-01-30 17:52:48 +1100

    TestSite: Set the static_url_path and static_folder to web/app when creating Flask instance

--- test_site/app.py
+++ test_site/app.py
@@ -5,7 +5,7 @@ from dstore import MemoryStore
 
 
 def create_app( name = __name__, config = "tests.configs.unit_tests", store = MemoryStore ):
-    app = Flask( name )
+    app = Flask( name, static_url_path = "", static_folder = "web/app" )
     app.config.from_object( config )
     api = API( store( models ), app )
 
