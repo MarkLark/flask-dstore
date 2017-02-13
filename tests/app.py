1708cb8ab2a07ab0c6ac745c1183c8eb5ee1bdcb
MarkLark <mark.pittaway@mlit.net.au>
2017-02-13 13:28:10 +1100

    Tests: Removed test_site template views and run-test-site.py script

--- tests/app.py
+++ tests/app.py
@@ -9,14 +9,4 @@ def create_app( name = __name__, config = "tests.configs.unit_tests", store = Me
     app.config.from_object( config )
     api = API( store( models ), app )
 
-    app.add_url_rule( "/",     "index", index )
-    app.add_url_rule( "/crud", "crud",  crud  )
     return app, api
-
-
-def index():
-    return render_template( "index.html" )
-
-
-def crud():
-    return render_template( "crud.html" )
